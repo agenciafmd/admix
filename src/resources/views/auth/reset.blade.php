48199763033c77aebe0913b0d44d5023dd57b68c
Irineu Junior <irineu@fmd.ag>
2019-11-06 14:06:45 -0300

    feat(logo): corrige logo do reset

--- src/resources/views/auth/reset.blade.php
+++ src/resources/views/auth/reset.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col col-login mx-auto">
                     <div class="text-center mb-6">
-                        <img src="/images/admix-logo.svg" class="h-6" alt="">
+                        <img src="/images/fmd.svg" class="h-6" alt="">
                     </div>
                     <form class="card" action="{{ route('admix.recover.reset') }}" method="post">
                         {{ csrf_field() }}
