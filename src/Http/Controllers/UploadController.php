d6642da23608c19581033d2c534c1a7b10609f19
Irineu Junior <irineu@fmd.ag>
2020-03-04 17:16:45 -0300

    feat(upload): permite multiplas descrições

--- src/Http/Controllers/UploadController.php
+++ src/Http/Controllers/UploadController.php
@@ -3,7 +3,6 @@
 namespace Agenciafmd\Admix\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Collective\Html\FormFacade as Form;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -63,35 +62,65 @@ class UploadController extends Controller
         $media = Media::where('custom_properties->uuid', $uuid)
             ->first();
 
+        $modelName = strtolower(class_basename($media->model_type));
+        $collectionName = $media->collection_name;
+
         $html = '<form method="POST" action="' . route('admix.upload.meta.post', $uuid) . '" accept-charset="UTF-8" id="formUploadMetaPost" onsubmit="return event.preventDefault();">';
         $html .= '<input name="_token" type="hidden" value="' . csrf_token() . '">';
 
-        try {
-            foreach (app()->make('languages') as $k => $language) {
-                $html .= '<li class="list-group-item">
-                    <div class="row gutters-sm">
-                        <label for="' . $language->name . '" class="col-xl-3 col-form-label pt-0 pt-xl-2">' . $language->name . '</label>
-                        <div class="col-xl-5">
-                            <input class="form-control" name="meta[' . $language->locale . ']" type="text" value="' . (($media->getCustomProperty("meta.{$language->locale}")) ?? '') . '">
-                            <div class="invalid-feedback">
-                                o campo ' . strtolower($language->name) .' é obrigatório
+        if (config("upload-configs.{$modelName}.{$collectionName}.meta")) {
+            foreach (config("upload-configs.{$modelName}.{$collectionName}.meta") as $field) {
+                $html .= '<li class="list-group-item mb-4">
+                        <div class="row gutters-sm">
+                            <label for="Descrição" class="col-xl-3 col-form-label pt-0 pt-xl-2">' . ucfirst($field['label']) . '</label>
+                            <div class="col-xl-5">';
+                if (isset($field['options']) && is_array($field['options'])) {
+                    $html .= '<select class="form-control" name="meta[' . $field['name'] . ']">
+                        <option value="">-</option>';
+                    foreach ($field['options'] as $option) {
+                        $html .= '<option value="' . $option . '" ' . (($media->getCustomProperty("meta." . $field['name']) == $option) ? 'selected' : '') . '>' . $option . '</option>';
+                    }
+                    $html .= '</select>';
+                } else {
+                    $html .= '<input class="form-control" name="meta[' . $field['name'] . ']" type="text" value="' . (($media->getCustomProperty("meta." . $field['name'])) ?? '') . '">';
+                }
+
+                $html .= '
+                                <div class="invalid-feedback">
+                                    o campo ' . strtolower($field['label']) . ' é obrigatório
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>';
+                    </li>';
             }
-        } catch (\Exception $e) {
-            $html .= '<li class="list-group-item">
-                    <div class="row gutters-sm">
-                        <label for="Descrição" class="col-xl-3 col-form-label pt-0 pt-xl-2">Descrição</label>
-                        <div class="col-xl-5">
-                            <input class="form-control" name="meta[' . app()->getLocale() . ']" type="text" value="' . (($media->getCustomProperty("meta." . app()->getLocale())) ?? '') . '">
-                            <div class="invalid-feedback">
-                                o campo descrição é obrigatório
+        } else {
+            try {
+                foreach (app()->make('languages') as $k => $language) {
+                    $html .= '<li class="list-group-item">
+                        <div class="row gutters-sm">
+                            <label for="' . $language->name . '" class="col-xl-3 col-form-label pt-0 pt-xl-2">' . $language->name . '</label>
+                            <div class="col-xl-5">
+                                <input class="form-control" name="meta[' . $language->locale . ']" type="text" value="' . (($media->getCustomProperty("meta.{$language->locale}")) ?? '') . '">
+                                <div class="invalid-feedback">
+                                    o campo ' . strtolower($language->name) . ' é obrigatório
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>';
+                    </li>';
+                }
+            } catch (\Exception $e) {
+                $html .= '<li class="list-group-item">
+                        <div class="row gutters-sm">
+                            <label for="Descrição" class="col-xl-3 col-form-label pt-0 pt-xl-2">Descrição</label>
+                            <div class="col-xl-5">
+                                <input class="form-control" name="meta[' . app()->getLocale() . ']" type="text" value="' . (($media->getCustomProperty("meta." . app()->getLocale())) ?? '') . '">
+                                <div class="invalid-feedback">
+                                    o campo descrição é obrigatório
+                                </div>
+                            </div>
+                        </div>
+                    </li>';
+            }
         }
 
         $html .= '</form>';
