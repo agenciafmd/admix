469dd4d8c2a09c82369a8895b9fc512371d8c76c
Irineu Junior <irineu@fmd.ag>
2020-09-02 16:58:48 -0300

    feat(admix): criação do componente is-active

---
+++ src/Http/Livewire/IsActive.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Agenciafmd\Admix\Http\Livewire;
+
+use Livewire\Component;
+
+class IsActive extends Component
+{
+    public $item;
+    public $isActive = 0;
+
+    public function mount($myModel, $myId)
+    {
+        $model = new $myModel();
+        $this->item = $model->where('id', $myId)
+            ->first();
+        $this->isActive = $this->item->is_active;
+    }
+
+    public function render()
+    {
+        return view('agenciafmd/admix::livewire.is-active');
+    }
+
+    public function update($value)
+    {
+        $this->item->is_active = $value;
+        $this->item->save();
+    }
+}
