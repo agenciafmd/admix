4b5df4c21748ff251efb51c9d152265bb8e84edf
Irineu Junior <irineu@fmd.ag>
2020-09-23 11:22:30 -0300

    feat(form): remove o form

--- src/resources/views/partials/btn/restore.blade.php
+++ src/resources/views/partials/btn/restore.blade.php
@@ -1,6 +1,7 @@
-{{ Form::open(['url' => $url, 'method' => 'post', 'class' => 'restore', 'id' => 'formRestore' . md5($url)]) }}
-<a role="button" class="js-dimmer js-submit">
-    <i class="icon fe-refresh-cw text-muted d-md-none"></i>
-    <span class="d-none d-md-inline-block">Restaurar</span>
-</a>
-{{ Form::close() }}
+<form action="{{ $url }}" method="POST" class="restore" id="formRestore{{ md5($url) }}">
+    @csrf
+    <a role="button" class="js-dimmer js-submit">
+        <i class="icon fe-refresh-cw text-muted d-md-none"></i>
+        <span class="d-none d-md-inline-block">Restaurar</span>
+    </a>
+</form>
