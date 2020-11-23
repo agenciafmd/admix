d13c39d557c3ce75944a30363eb2e309d1a55059
Irineu Junior <irineu@mixd.com.br>
2020-11-23 13:40:08 -0300

    feat(admix): try catch no TurboTrait.php

--- src/TurboTrait.php
+++ src/TurboTrait.php
@@ -14,44 +14,64 @@ trait TurboTrait
         }
 
         static::created(function ($model) {
-            if ($model->is_active) {
-                dispatch(function () use ($model) {
-                    with(new GuzzleClient())->request('GET', $model->url);
-                })
-                    ->delay(now()->addSeconds(5))
-                    ->onQueue('low');
+            try {
+                if ($model->is_active) {
+                    dispatch(function () use ($model) {
+                        (new GuzzleClient)->request('GET', $model->url);
+                    })
+                        ->delay(now()->addSeconds(5))
+                        ->onQueue('low');
+                }
+            } catch (\Exception $e) {
+                // não tem problema
             }
         });
 
         static::saved(function ($model) {
-            if ($model->is_active) {
-                dispatch(function () use ($model) {
-                    Artisan::call('page-cache:clear', ['slug' => str_replace(config('app.url'), '', $model->url)]);
+            try {
+                if ($model->is_active) {
+                    dispatch(function () use ($model) {
+                        Artisan::call('page-cache:clear', [
+                            'slug' => str_replace(config('app.url'), '', $model->url),
+                        ]);
 
-                    with(new GuzzleClient())->request('GET', $model->url);
-                })
-                    ->delay(now()->addSeconds(5))
-                    ->onQueue('low');
+                        (new GuzzleClient)->request('GET', $model->url);
+                    })
+                        ->delay(now()->addSeconds(5))
+                        ->onQueue('low');
+                }
+            } catch (\Exception $e) {
+                // não tem problema
             }
         });
 
         static::deleted(function ($model) {
-            if ($model->is_active) {
-                dispatch(function () use ($model) {
-                    Artisan::call('page-cache:clear', ['slug' => str_replace(config('app.url'), '', $model->url)]);
-                })
-                    ->delay(now()->addSeconds(5))
-                    ->onQueue('low');
+            try {
+                if ($model->is_active) {
+                    dispatch(function () use ($model) {
+                        Artisan::call('page-cache:clear', [
+                            'slug' => str_replace(config('app.url'), '', $model->url),
+                        ]);
+                    })
+                        ->delay(now()->addSeconds(5))
+                        ->onQueue('low');
+                }
+            } catch (\Exception $e) {
+                // não tem problema
             }
         });
 
         static::restored(function ($model) {
-            if ($model->is_active) {
-                dispatch(function () use ($model) {
-                    with(new GuzzleClient())->request('GET', $model->url);
-                })
-                    ->delay(now()->addSeconds(5))
-                    ->onQueue('low');
+            try {
+                if ($model->is_active) {
+                    dispatch(function () use ($model) {
+                        (new GuzzleClient)->request('GET', $model->url);
+                    })
+                        ->delay(now()->addSeconds(5))
+                        ->onQueue('low');
+                }
+            } catch (\Exception $e) {
+                // não tem problema
             }
         });
     }
