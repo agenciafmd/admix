83e1319873c00ee08170a8f9f3a758b5f526ca68
Irineu Junior <irineu@mixd.com.br>
2021-01-07 11:06:24 -0300

    fix(media) remove o return do saved

--- src/Observers/UserObserver.php
+++ src/Observers/UserObserver.php
@@ -8,32 +8,6 @@ class UserObserver
 {
     public function saved(User $user)
     {
-        $this->media($user);
-    }
-
-    private function media($model)
-    {
-        $request = request();
-
-        if (!$request->media) {
-            return false;
-        }
-
-        foreach ($request->media as $media) {
-            if (is_array($media['collection'])) {
-                $collection = reset($media['collection']);
-                $file = storage_path('admix/tmp') . "/" . reset($media['name']);
-
-                $model->doUploadMultiple($file, $collection);
-
-            } else {
-                $collection = $media['collection'];
-                $file = storage_path('admix/tmp') . "/{$media['name']}";
-
-                $model->doUpload($file, $collection);
-            }
-        }
-
-        return true;
+        //
     }
 }
