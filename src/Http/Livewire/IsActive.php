28508f05ce4755e25acbaa685a143629859c35a6
Irineu Junior <irineu@fmd.ag>
2022-07-14 10:49:39 -0300

    feat(isActive) permite que o isActive seja setado a fim e evitar sobrecarga no banco

--- src/Http/Livewire/IsActive.php
+++ src/Http/Livewire/IsActive.php
@@ -8,14 +8,22 @@ class IsActive extends Component
 {
     public $item;
     public $isActive = 0;
+    public $myModel;
+    public $myId;
 
-    public function mount($myModel, $myId)
+    public function mount($myModel, $myId, $isActive = null)
     {
-        $model = new $myModel();
-        $this->item = $model->withTrashed()
-            ->where('id', $myId)
-            ->first();
-        $this->isActive = $this->item->is_active;
+        $this->myModel = new $myModel();
+        $this->myId = $myId;
+
+        if ($isActive) {
+            $this->isActive = $isActive;
+        } else {
+            $this->item = $this->myModel->withTrashed()
+                ->where('id', $this->myId)
+                ->first();
+            $this->isActive = $this->item->is_active;
+        }
     }
 
     public function render()
@@ -25,6 +33,9 @@ class IsActive extends Component
 
     public function update($value)
     {
+        $this->item = $this->myModel->withTrashed()
+            ->where('id', $this->myId)
+            ->first();
         $this->item->is_active = $value;
         $this->item->save();
     }
