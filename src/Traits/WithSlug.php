dcf6bdffc9108a6b1035de5d6b9242890a8c4080
Irineu Junior <irineu@fmd.ag>
2023-11-29 15:08:22 -0300

    feat(withSlug) create the trait

---
+++ src/Traits/WithSlug.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Agenciafmd\Admix\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+trait WithSlug
+{
+    protected static function bootWithSlug(): void
+    {
+        static::saving(function (Model $model) {
+            if ($slug = $model->generateSlug()) {
+                $model->slug = $slug;
+            }
+        });
+    }
+
+    protected function generateSlug(): ?string
+    {
+        if ($this->getRawOriginal('name') === $this->name) {
+            return null;
+        }
+
+        $slug = Str::of($this->name)
+            ->trim()
+            ->limit(120)
+            ->slug();
+
+        $i = 1;
+        $uniqueSlug = $slug;
+        while (static::withoutGlobalScopes()
+            ->where('slug', $uniqueSlug)
+            ->orderBy('slug')
+            ->exists()) {
+            $uniqueSlug = $slug . '-' . $i++;
+        }
+
+        return $uniqueSlug;
+    }
+}
