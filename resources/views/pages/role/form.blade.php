456e97ec5fc656a181e045c3ede23d317055c901
Irineu Junior <irineu@fmd.ag>
2025-01-31 14:47:30 -0300

    feat(form) adiciona a quebra de linha nos parametros

--- resources/views/pages/role/form.blade.php
+++ resources/views/pages/role/form.blade.php
@@ -2,13 +2,15 @@
         title="{{ $role->exists ? __('Update :name', ['name' => __(config('admix.role.name'))]) : __('Create :name', ['name' => __(config('admix.role.name'))]) }}">
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.label for="form.is_active">
+            <x-form.label
+                    for="form.is_active">
                 {{ str(__('admix::fields.is_active'))->ucfirst() }}
             </x-form.label>
-            <x-form.toggle name="form.is_active"
-                           :large="true"
-                           :label-on="__('Yes')"
-                           :label-off="__('No')"
+            <x-form.toggle
+                    name="form.is_active"
+                    :large="true"
+                    :label-on="__('Yes')"
+                    :label-off="__('No')"
             />
         </div>
         <div class="col-md-6 mb-3">
@@ -16,24 +18,31 @@
     </div>
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.input name="form.name" :label="__('admix::fields.name')"/>
+            <x-form.input
+                    name="form.name"
+                    :label="__('admix::fields.name')"
+            />
         </div>
         <div class="col-md-6 mb-3">
         </div>
     </div>
     <div class="row">
         <div class="col-md-12 mb-3">
-            <x-form.label for="role.rules">
+            <x-form.label
+                    for="role.rules">
                 {{ str(__('admix::fields.rules'))->ucfirst() }}
             </x-form.label>
             <div class="row">
                 @foreach ($gateRules as $rule)
                     <div class="col-md-3">
-                        <label class="form-label">{{ $rule['name'] }}</label>
+                        <label class="form-label">
+                            {{ $rule['name'] }}
+                        </label>
                         @foreach($rule['policies'] as $policy)
-                            <x-form.toggle name="form.rules"
-                                           :label="$policy['name']"
-                                           :value="$policy['policy']"
+                            <x-form.toggle
+                                    name="form.rules"
+                                    :label="$policy['name']"
+                                    :value="$policy['policy']"
                             />
                         @endforeach
                     </div>
@@ -44,16 +53,22 @@
     <x-slot:complement>
         @if($role->exists)
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.id')"
-                                  :value="$role->id"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.id')"
+                        :value="$role->id"
+                />
             </div>
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.created_at')"
-                                  :value="$role->created_at->format(config('admix.timestamp.format'))"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.created_at')"
+                        :value="$role->created_at->format(config('admix.timestamp.format'))"
+                />
             </div>
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.updated_at')"
-                                  :value="$role->updated_at->format(config('admix.timestamp.format'))"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.updated_at')"
+                        :value="$role->updated_at->format(config('admix.timestamp.format'))"
+                />
             </div>
         @endif
     </x-slot:complement>
--- resources/views/pages/user/form.blade.php
+++ resources/views/pages/user/form.blade.php
@@ -2,32 +2,42 @@
         title="{{ $user->exists ? __('Update :name', ['name' => __(config('admix.user.name'))]) : __('Create :name', ['name' => __(config('admix.user.name'))]) }}">
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.label for="form.is_active">
+            <x-form.label
+                    for="form.is_active">
                 {{ str(__('admix::fields.is_active'))->ucfirst() }}
             </x-form.label>
-            <x-form.toggle name="form.is_active"
-                           :large="true"
-                           :label-on="__('Yes')"
-                           :label-off="__('No')"
+            <x-form.toggle
+                    name="form.is_active"
+                    :large="true"
+                    :label-on="__('Yes')"
+                    :label-off="__('No')"
             />
         </div>
         <div class="col-md-6 mb-3">
-            <x-form.label for="form.can_notify">
+            <x-form.label
+                    for="form.can_notify">
                 {{ str(__('admix::fields.can_notify'))->ucfirst() }}
             </x-form.label>
-            <x-form.toggle name="form.can_notify"
-                           :large="true"
-                           :label-on="__('Yes')"
-                           :label-off="__('No')"
+            <x-form.toggle
+                    name="form.can_notify"
+                    :large="true"
+                    :label-on="__('Yes')"
+                    :label-off="__('No')"
             />
         </div>
     </div>
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.input name="form.name" :label="__('admix::fields.name')"/>
+            <x-form.input
+                    name="form.name"
+                    :label="__('admix::fields.name')"
+            />
         </div>
         <div class="col-md-6 mb-3">
-            <x-form.input name="form.email" :label="__('admix::fields.email')"/>
+            <x-form.input
+                    name="form.email"
+                    :label="__('admix::fields.email')"
+            />
         </div>
     </div>
     <div class="row">
@@ -43,40 +53,49 @@
     </div>
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.password name="form.password"
-                             :label="__('admix::fields.password')"
-                             wire:model.defer="form.password"
+            <x-form.password
+                    name="form.password"
+                    :label="__('admix::fields.password')"
+                    wire:model.defer="form.password"
             />
         </div>
         <div class="col-md-6 mb-3">
-            <x-form.password name="form.password_confirmation"
-                             :label="__('admix::fields.password_confirmation')"
-                             wire:model.defer="form.password_confirmation"
+            <x-form.password
+                    name="form.password_confirmation"
+                    :label="__('admix::fields.password_confirmation')"
+                    wire:model.defer="form.password_confirmation"
             />
         </div>
     </div>
     <div class="row">
         <div class="col-md-6 mb-3">
-            <x-form.select name="form.role_id"
-                           :label="__('admix::fields.role_id')"
-                           :options="$roleOptions"
-                           :placeholder="__('Administrator')"
+            <x-form.select
+                    name="form.role_id"
+                    :label="__('admix::fields.role_id')"
+                    :options="$roleOptions"
+                    :placeholder="__('Administrator')"
             />
         </div>
     </div>
     <x-slot:complement>
         @if($user->exists)
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.id')"
-                                  :value="$user->id"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.id')"
+                        :value="$user->id"
+                />
             </div>
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.created_at')"
-                                  :value="$user->created_at->format(config('admix.timestamp.format'))"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.created_at')"
+                        :value="$user->created_at->format(config('admix.timestamp.format'))"
+                />
             </div>
             <div class="mb-3">
-                <x-form.plaintext :label="__('admix::fields.updated_at')"
-                                  :value="$user->updated_at->format(config('admix.timestamp.format'))"/>
+                <x-form.plaintext
+                        :label="__('admix::fields.updated_at')"
+                        :value="$user->updated_at->format(config('admix.timestamp.format'))"
+                />
             </div>
         @endif
     </x-slot:complement>
