b3979cfe145d9a92d64f3f76547a3bcda0ab0e9e
Carlos Tamagawa <carlos@mixd.com.br>
2025-04-22 11:15:38 -0300

    fix(menu) acrescenta o visible no component menu item

--- resources/views/components/aside/item.blade.php
+++ resources/views/components/aside/item.blade.php
@@ -1,19 +1,22 @@
 @props([
     'icon' => '',
-    'active' => '',
+    'active' => false,
     'label' => '',
     'url' => '',
+    'visible' => true,
 ])
-<li @class([
-        'nav-item',
-        'active' => $active,
-    ])>
-    <a href="{{ $url }}" class="nav-link">
-        <span class="nav-link-icon d-md-none d-lg-inline-block">
-            <x-tblr-icon name="{{ $icon }}"/>
-        </span>
-        <span class="nav-link-title">
-            {{ $label }}
-        </span>
-    </a>
-</li>
+@if($visible)
+    <li @class([
+            'nav-item',
+            'active' => $active,
+        ])>
+        <a href="{{ $url }}" class="nav-link">
+            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                <x-tblr-icon name="{{ $icon }}"/>
+            </span>
+            <span class="nav-link-title">
+                {{ $label }}
+            </span>
+        </a>
+    </li>
+@endif
