e82792f1fe438f6f09f7ba35f0f399399f72d131
Irineu Junior <irineu@fmd.ag>
2025-05-06 14:18:49 -0300

    feat(admix): ajuste caminho e inclusão novos ícones tabler

--- resources/mail/icon.blade.php
+++ resources/mail/icon.blade.php
@@ -1,7 +1,7 @@
 <table class="icon icon-lg bg-{{ $color }}-lightest " cellspacing="0" cellpadding="0">
     <tr>
         <td valign="middle" align="center">
-            <img src="{{ asset("vendor/admix/vendor/tabler/images/icons/{$color}/{$slot}.png") }}" class="va-middle"
+            <img src="{{ asset("vendor/admix-ui/tabler/dist/img/icons/{$color}/{$slot}.png") }}" class="va-middle"
                  width="40" height="40" alt="{{ $slot }}"/>
         </td>
     </tr>
